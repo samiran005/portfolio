7138334a5be0134a4e8b2780e10228ff2c1aba5c
samiran005 <maitysamiran442@gmail.com>
2022-11-20 14:29:00 +0530

    this is first

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,20 @@
-const bars = document.getElementById("bars");
+const bars = document.getElementById("menuClick");
+
+const menuIcon = document.querySelector(".menu-icon");
+const menuXicon = document.querySelector(".menu-xicon");
 
 const menuBar = document.getElementById("menu-bar");
 console.log(menuBar)
 
 bars.addEventListener('click',()=>{
     menuBar.classList.toggle("display-flex");
-    if(menuBar.classList.contains("display-flex")){
-        bars.style.backgroundImage = "url(../images/xmark-solid.svg)";
-    }else {
-        bars.style.backgroundImage = "url(../images/bars-solid.svg)";
-    }
+    menuIcon.classList.toggle("display-flex");
+    menuXicon.classList.toggle("display-flex");
+    // if(menuBar.classList.contains("display-flex")){
+    //     bars.style.backgroundImage = "url(../images/xmark-solid.svg)";
+    // }else {
+    //     bars.style.backgroundImage = "url(../images/bars-solid.svg)";
+    // }
 })
 
 document.addEventListener('scroll',()=>{
