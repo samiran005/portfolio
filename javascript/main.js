5f1c721efa074f6cbf581ddc4bbcec21ea8a6708
samiran005 <maitysamiran442@gmail.com>
2022-11-20 19:42:26 +0530

    this is first

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,8 @@ const bars = document.getElementById("menuClick");
 const menuIcon = document.querySelector(".menu-icon");
 const menuXicon = document.querySelector(".menu-xicon");
 
+const blurEffect = document.querySelector(".blur-for-menu-open");
+
 const menuBar = document.getElementById("menu-bar");
 console.log(menuBar)
 
@@ -10,14 +12,14 @@ bars.addEventListener('click',()=>{
     menuBar.classList.toggle("display-flex");
     menuIcon.classList.toggle("display-flex");
     menuXicon.classList.toggle("display-flex");
-    // if(menuBar.classList.contains("display-flex")){
-    //     bars.style.backgroundImage = "url(../images/xmark-solid.svg)";
-    // }else {
-    //     bars.style.backgroundImage = "url(../images/bars-solid.svg)";
-    // }
+    blurEffect.classList.toggle("display-flex");
+
 })
 
 document.addEventListener('scroll',()=>{
     menuBar.classList.remove("display-flex");
-    bars.style.backgroundImage = "url(../images/bars-solid.svg)";
+    menuIcon.classList.add("display-flex");
+    menuXicon.classList.remove("display-flex");
+    blurEffect.classList.remove("display-flex");
+
 })
